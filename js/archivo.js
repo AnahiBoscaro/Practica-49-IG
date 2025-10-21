71e546e229a93da2bda44f82054fecb7665728db
AnahiBoscaro <anahiboscaro3@gmail.com>
2025-10-21 11:21:19 -0300

    Agregué archivos al repositorio

---
+++ js/archivo.js
@@ -0,0 +1,40 @@
+console.log ("HOLA MUNDO!")
+
+let miNombre = "Anahi" ;
+let miApellido = "Boscaro" ;
+let nombreyapellido = miNombre + " " + miApellido;
+//console.log ( nombreyapellido )
+
+ //-----------------------
+
+let edad = 22;
+edad ++;
+//console.log (edad);
+
+
+const facultad = "UNA" ;
+//console.log (facultad)
+
+console.log ( "Feliz cumpleaños" + " " + nombreyapellido + "! " + edad + 
+" años no se cumplen todos los dias! Gracias por ser parte de la " + facultad)
+
+//-------------------------
+
+let kilopan = 4765.50
+let compra = 2
+console.log (kilopan * compra) 
+
+/* let kilopan = 4765.50
+console.log ( kilopan * 2 ) */
+
+//-------------------------
+
+let disco = 15000;
+let descuento = 18 ;
+let montoDescuento = disco * descuento / 100 ;
+let precioFinal = disco - montoDescuento ;
+
+console.log ( "Precio sin descuento es $" + disco + "\n" + "El descuento es de $" 
+	+ montoDescuento + "\n" + "El precio con descuento es $" + precioFinal );
+
+
